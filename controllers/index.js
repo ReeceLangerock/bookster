9c550235d5c454380b176b1b68046a8f78962149
ReeceLangerock <reecelangerock@gmail.com>
2017-03-05 20:00:01 -0600

    book trading system working

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
+//SETUP ROUTER
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
@@ -10,20 +11,23 @@ router.use(bodyParser.urlencoded({
 }));
 router.use(bodyParser.json());
 
+
 router.get('/', function(req, res) {
+    // Check if user is signed in, and if so get their books (needed for showing request button)
     if (req.isAuthenticated()) {
         var userBooksProm = getUserBooks(req.user.mongoID);
     } else {
         var userBooksProm = Promise.resolve(null);
     }
+    // get all the books
     var allBooksProm = getAllBooks();
-
 
 
     Promise.all([userBooksProm, allBooksProm]).then(function(responses, error) {
         userBooks = responses[0];
         allBooks = responses[1];
 
+        // send the user books along with all of the rest
         if (req.isAuthenticated()) {
             res.render('index', {
                 authenticatedUser: true,
@@ -33,6 +37,7 @@ router.get('/', function(req, res) {
             });
 
         } else {
+            // just send all the books
             res.render('index', {
                 authenticatedUser: false,
                 books: allBooks,
@@ -41,31 +46,36 @@ router.get('/', function(req, res) {
             });
         }
     })
-
 });
 
+// handle sending of trade requests
 router.post('/send-request', function(req, res) {
+
     var bookToSendID = req.body.bookToSendID
     var bookToReceiveID = req.body.bookToReceiveID;
     var bookOwnerID = req.body.bookOwnerID;
 
-
-    checkForRequest(req.user.mongoID, bookToReceiveID).then(function(response, error) {
-        if (response == "GOOD_TO_GO") {
-            var addRequestToSelfProm = addRequestToSelf(req.user.mongoID, bookToSendID, bookToReceiveID, bookOwnerID);
-            var sendRequestToOtherUserProm = sendRequestToOtherUser(req.user.mongoID, bookToSendID, bookToReceiveID, bookOwnerID);
+    // check if the user has already sent a trade offer for the selected book
+    var pendingRequestProm = checkForRequest("requestsPending.bookToReceive", req.user.mongoID, bookToReceiveID);
+    var sentRequestProm = checkForRequest("requestsSent.bookToReceive", req.user.mongoID, bookToReceiveID);
+    Promise.all([pendingRequestProm, sentRequestProm]).then(function(response, error) {
+        if (response[0] == "GOOD_TO_GO" && response[1] == "GOOD_TO_GO") {
+            //if the response wasn't found add a sent to request to current user and pending request to other user
+            var requestID = new ObjectID();
+            var addRequestToSelfProm = addRequestToSelf(req.user.mongoID, bookToSendID, bookToReceiveID, bookOwnerID, requestID);
+            var sendRequestToOtherUserProm = sendRequestToOtherUser(req.user.mongoID, bookToSendID, bookToReceiveID, bookOwnerID, requestID);
 
             Promise.all([addRequestToSelfProm, sendRequestToOtherUserProm]).then(function(responses, error) {
+                //if either update failed, send errr
                 if (responses[0] == "FAILED" || responses[1] == "FAILED") {
-
                     req.flash('error', 'Request failed')
                     res.redirect('back');
-                } else {
-                    req.flash('success', 'Request Send.');
+                } else { // otherwise send successful send
+                    req.flash('success', 'Request Sent.');
                     res.redirect('back');
                 }
             })
-
+            // if the request was found(already been sent) let user know
         } else {
             req.flash('error', 'Request already sent')
             res.redirect('back');
@@ -74,11 +84,12 @@ router.post('/send-request', function(req, res) {
 
 });
 
-function checkForRequest(id, bookToReceiveID) {
+// check for the request in the users sent request array
+function checkForRequest(sentOrPending, id, bookToReceiveID) {
     return new Promise(function(resolve, reject) {
         userModel.findOne({
                 _id: id,
-                "requestsSent.bookToReceive": [
+                [sentOrPending]: [
                     bookToReceiveID
                 ]
             },
@@ -95,14 +106,14 @@ function checkForRequest(id, bookToReceiveID) {
     });
 }
 
-function addRequestToSelf(id, bookToSendID, bookToReceiveID, bookOwnerID) {
+function addRequestToSelf(id, bookToSendID, bookToReceiveID, bookOwnerID, requestID) {
     return new Promise(function(resolve, reject) {
         userModel.findOneAndUpdate({
                 _id: id
             }, {
-                $push: {
+                $push: { // push trade data into current user userModel
                     requestsSent: {
-                        'requestID': new ObjectID(),
+                        'requestID': requestID,
                         'bookToSend': bookToSendID,
                         'bookToReceive': bookToReceiveID,
                         'bookOwnerID': bookOwnerID
@@ -119,14 +130,14 @@ function addRequestToSelf(id, bookToSendID, bookToReceiveID, bookOwnerID) {
     })
 }
 
-function sendRequestToOtherUser(id, bookToSendID, bookToReceiveID, bookOwnerID) {
+function sendRequestToOtherUser(id, bookToSendID, bookToReceiveID, bookOwnerID, requestID) {
     return new Promise(function(resolve, reject) {
         userModel.findOneAndUpdate({
                 _id: bookOwnerID
             }, {
                 $push: {
-                    requestsPending: {
-                        'requestID': new ObjectID(),
+                    requestsPending: { //push trade data into other users userModel
+                        'requestID': requestID,
                         'bookToSend': bookToReceiveID, // these are flipped from requestPending
                         'bookToReceive': bookToSendID, // these are flipped from requestPending
                         'bookOwnerID': id
@@ -142,7 +153,6 @@ function sendRequestToOtherUser(id, bookToSendID, bookToReceiveID, bookOwnerID) 
             });
     })
 }
-
 
 function getAllBooks() {
     return new Promise(function(resolve, reject) {
@@ -172,7 +182,5 @@ function getUserBooks(userID) {
 
     });
 }
-
-
 
 module.exports = router;
--- controllers/trades.js
+++ controllers/trades.js
@@ -15,18 +15,23 @@ router.get('/', function(req, res) {
     getUser(req.user.mongoID).then(function(response, error) {
         var requestSentBookToOfferArray = [];
         var requestSentBookToReceiveArray = [];
+        var requestSentIdArray = [];
         var requestPendingBookToReceiveArray = [];
         var requestPendingBookToOfferArray = [];
+        var requestPendingArray = [];
+
         for (let i = 0; i < response.requestsSent.length; i++) {
 
             requestSentBookToOfferArray.push(response.requestsSent[i].bookToSend);
             requestSentBookToReceiveArray.push(response.requestsSent[i].bookToReceive);
+            requestSentIdArray.push(response.requestsSent[i].requestID);
         }
 
         for (let i = 0; i < response.requestsPending.length; i++) {
 
             requestPendingBookToOfferArray.push(response.requestsPending[i].bookToSend);
             requestPendingBookToReceiveArray.push(response.requestsPending[i].bookToReceive);
+            requestPendingArray.push(response.requestsPending[i]);
         }
 
 
@@ -42,8 +47,10 @@ router.get('/', function(req, res) {
                     authenticatedUser: true,
                     requestSentBooksOffered: responses[0],
                     requestSentBooksReceiving: responses[1],
+                    requestSentId: requestSentIdArray,
                     requestPendingBooksOffered: responses[2],
                     requestPendingBooksReceiving: responses[3],
+                    requestPendingArray: requestPendingArray,
                     userID: req.user.mongoID
                 });
 
@@ -51,14 +58,121 @@ router.get('/', function(req, res) {
                 res.send('error');
             }
         })
-
-
-
     })
 
-
-
 });
+
+router.post('/cancel', function(req, res) {
+    removeRequest('requestsSent.requestID', 'requestsSent', req.body.requestID);
+    removeRequest('requestsPending.requestID', 'requestsPending', req.body.requestID);
+
+    res.send('cancel');
+})
+
+router.post('/decline', function(req, res) {
+    console.log(req.body);
+})
+
+router.post('/accept', function(req, res) {
+    var bookToReceive = req.body.bookToReceive;
+    var bookToSend = req.body.bookToSend;
+    var otherUsersID = req.body.otherUserID;
+    var requestID = req.body.requestID;
+    var acceptProm1 = acceptRequest(req.user.mongoID, bookToSend, bookToReceive);
+    var acceptProm2 = acceptRequest(otherUsersID, bookToReceive, bookToSend);
+
+    var swapProm1 = swapBookOwnership(bookToSend, otherUsersID);
+    var swapProm2 = swapBookOwnership(bookToReceive, req.user.mongoID);
+
+    Promise.all([acceptProm1,acceptProm2,swapProm1,swapProm2]).then(function(response, error){
+      removeRequest('requestsSent.requestID', 'requestsSent', req.body.requestID);
+      removeRequest('requestsPending.requestID', 'requestsPending', req.body.requestID);
+    })
+
+})
+
+function acceptRequest(userID, bookToSend, bookToReceive) {
+  return new Promise(function(resolve, reject) {
+      userModel.findOneAndUpdate({
+              _id: userID
+          }, {
+              $pull: {
+                  books: bookToSend
+              }
+          }, {
+            $push: {
+                books: bookToReceive
+              }
+          },
+          function(err, doc) {
+              if (err) {
+                  console.log(err)
+                  console.log('failed')
+                  reject('FAILED');
+              } else if (doc) {
+                console.log('swapped')
+                  resolve("SWAPPED");
+              } else {
+                console.log('NOT_FOUND')
+                  resolve("NOT_FOUND");
+              }
+          })
+  });
+}
+
+function swapBookOwnership(bookID, newOwner) {
+  return new Promise(function(resolve, reject) {
+      bookModel.findOneAndUpdate({
+              _id: bookID
+          }, {
+              $set: {
+                  ownedBy: newOwner
+              }
+          },
+          function(err, doc) {
+              if (err) {
+                  console.log(err)
+                  console.log('failed')
+                  reject('FAILED');
+              } else if (doc) {
+                console.log('swapped')
+                  resolve("SWAPPED");
+              } else {
+                console.log('NOT_FOUND')
+                  resolve("NOT_FOUND");
+              }
+          })
+  });
+}
+
+
+
+function removeRequest(sentOrPendingID, sentOrPending, requestID) {
+    return new Promise(function(resolve, reject) {
+        userModel.findOneAndUpdate({
+                [sentOrPendingID]: [requestID]
+            }, {
+                $pull: {
+                    [sentOrPending]: {
+                        requestID: requestID
+                    }
+                }
+            },
+            function(err, doc) {
+                if (err) {
+                    console.log(err)
+                    reject('FAILED');
+                } else if (doc) {
+                    console.log(doc)
+                    resolve("REMOVED");
+                } else {
+                    resolve("NOT_FOUND");
+                }
+            })
+    });
+}
+
+
 
 function getRequestedBooks(bookID) {
     return new Promise(function(resolve, reject) {
